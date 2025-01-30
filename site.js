69550ae7e63c24952f4a49e07960f136c41e6759
Yang <300100655@fvtc.edu>
2025-01-30 01:52:57 -0600

    finished homepage

---
+++ site.js
@@ -0,0 +1,21 @@
+
+document.body.style.backgroundColor = 'lightgray'
+document.title = "Thane's HomePage"
+
+const image = document.querySelector('h2')
+image.style.border = '1px solid red'
+
+const firstP = document.querySelector('p')
+console.log(firstP)
+firstP.textContent = 'My name is Thane and I am still new to the programming area of my program. You are probably wondering how and why I got here. Knowledge and Passion is why, knowledge I wish to benefit my dream job and passion that inspires my dream job. This dream job I speak of is the livelyhood of a successfully creative author.'
+
+const mainContentSection = document.querySelector('p')
+mainContentSection.style.color = 'darkblue'
+
+const font = document.getElementById("fontChange")
+font.style.fontFamily = "Arial, sans-serif";
+
+/* const fontOl = document.getElementById("fontForOl")
+fontO1.style.fontFamily = "Arial, sans-serif"; */
+
+const link = document.querySelector('footer a')
